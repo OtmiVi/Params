5291ca5ace8015f3b88a69499fb90845ca30e7d3
YuraKyziv <37220376+OtmiVi@users.noreply.github.com>
2019-07-12 18:17:19 +0300

    Add files via upload

---
+++ Params/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int [] {1,3,4,4,4,2};
+            Sum(x);
+            Sum(2, 3, 4, 5, 67, 1);
+            Console.ReadKey();
+        }
+        static void Sum(params int[] x)
+        {
+            int sum = 0;
+            foreach (int s in x) 
+                sum += s;
+
+            Console.WriteLine( sum);
+        }
+    }
+}
